2c3c5a439635e9ed0aef9c134f4be87717a68a2c
Anya <ania.leonova@gmail.com>
2022-03-26 10:46:15 -0700

    Lesson 5. Completed parallax effect

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class ParallaxEffect : MonoBehaviour
 {
     public float speed = 5f;
+    public float bgLength;
+    public float bgStartPos;
+    private Vector2 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgStartPos = transform.position.x;
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        bgLength = sprite.bounds.size.x;
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        bool shouldLoopBackground = transform.position.x <= (bgStartPos - bgLength);
+        if (shouldLoopBackground)
+        {
+            transform.position = startPosition;
+        }
     }
 }
